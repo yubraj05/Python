1c0f93f2c039d290403e8903ad972baefa89eb26
yubraj05 <97792707+yubraj05@users.noreply.github.com>
2026-09-09 01:39:49 +0530

    Delete Python directory

--- Python/Advanced/CCT3.py
+++
@@ -1,5 +0,0 @@
-# Add numbers in two lists 
-a = [1,2,3]
-b = [4,5,6]
-print([a+b for a,b in zip(a,b)])
-
--- Python/Important Concepts/Yield method.py
+++
@@ -1,8 +0,0 @@
-def rep(a):
-    for i in range(a):
-        yield i
-
-
-x = list(rep(10))
-y = list(range(10))
-print(*x , *y )
